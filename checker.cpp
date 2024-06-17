42b63ff75e5c5e5f067e56a92cb17554bb559e16
linburk <156449179+linburk@users.noreply.github.com>
2024-06-17 16:33:01 +0300

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,4 +1,6 @@
+#include <cstdio>
 #include <cstdlib>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -15,13 +17,19 @@ std::string parseBeforeDot(std::string parsedStr) {
 }
 
 int main(int argc, char *argv[]) {
-  // argv[1] = iterations
-  // argv[2] = "work.cpp"
-  // argv[3] = "check.cpp"
-  // argv[4] = "gen.cpp"
-  // argv[5] = "work.txt"
-  // argv[6] = "check.txt"
-  // argv[7] = "test.txt"
+  // argv[1] = FILEPATH
+  // argv[2] = iterations
+  // argv[3] = "work.cpp"
+  // argv[4] = "check.cpp"
+  // argv[5] = "gen.cpp"
+  // argv[6] = "work.txt"
+  // argv[7] = "check.txt"
+  // argv[8] = "test.txt"
+  // argv[9] = create settings if exist
+
+  size_t iterations;
+  std::string FILEPATH;
+  std::string settings = "settings.txt";
   std::string commandCompileWork, commandCompileCheck, commandCompileGen;
   std::string commandLaunchWork, commandLaunchCheck, commandLaunchGen;
   std::string commandRmWork, commandRmCheck, commandRmGen;
@@ -29,13 +37,26 @@ int main(int argc, char *argv[]) {
   std::string argvWork, argvCheck, argvGen;
   std::string argvWorkT, argvCheckT, argvGenT;
 
-  int iterations = atoi(argv[1]);
-  argvWork = std::string(argv[2]);
-  argvCheck = std::string(argv[3]);
-  argvGen = std::string(argv[4]);
-  argvWorkT = std::string(argv[5]);
-  argvCheckT = std::string(argv[6]);
-  argvGenT = std::string(argv[7]);
+  FILEPATH = std::string(argv[1]);
+  iterations = atoi(argv[2]);
+  if (std::filesystem::exists(FILEPATH + settings)) {
+    std::ifstream input(FILEPATH + settings);
+    input >> argvWork >> argvCheck >> argvGen;
+    input >> argvWorkT >> argvCheckT >> argvGenT;
+  } else {
+    argvWork = std::string(argv[3]);
+    argvCheck = std::string(argv[4]);
+    argvGen = std::string(argv[5]);
+    argvWorkT = std::string(argv[6]);
+    argvCheckT = std::string(argv[7]);
+    argvGenT = std::string(argv[8]);
+    if (argc >= 9) {
+      std::ofstream setSettings(FILEPATH + settings);
+      setSettings << argvWork << "\n" << argvCheck << "\n" << argvGen << "\n";
+      setSettings << argvWorkT << "\n" << argvCheckT << "\n" << argvGenT;
+      std::cout << "Settings set\n";
+    }
+  }
 
   std::ifstream workOutput, checkOutput;
   workOutput.open(argvWorkT);
@@ -45,17 +66,22 @@ int main(int argc, char *argv[]) {
   checkName = parseBeforeDot(argvCheck);
   genName = parseBeforeDot(argvGen);
 
-  commandCompileWork = "clang++ " + argvWork + " -o " + workName;
-  commandCompileCheck = "clang++ " + argvCheck + " -o " + checkName;
-  commandCompileGen = "clang++ " + argvGen + " -o " + genName;
+  commandCompileWork =
+      "clang++ " + FILEPATH + argvWork + " -std=c++20 " + " -o " + FILEPATH + workName;
+  commandCompileCheck =
+      "clang++ " + FILEPATH + argvCheck + " -std=c++20 " + " -o " + FILEPATH + checkName;
+  commandCompileGen =
+      "clang++ " + FILEPATH + argvGen + " -std=c++20 " + " -o " + FILEPATH + genName;
 
-  commandLaunchWork = "./" + workName + " < " + argvGenT + " > " + argvWorkT;
-  commandLaunchCheck = "./" + checkName + " < " + argvGenT + " > " + argvCheckT;
-  commandLaunchGen = "./" + genName + " > " + argvGenT;
+  commandLaunchWork = FILEPATH + "./" + workName + " < " + FILEPATH + argvGenT +
+                      " > " + FILEPATH + argvWorkT;
+  commandLaunchCheck = FILEPATH + "./" + checkName + " < " + FILEPATH +
+                       argvGenT + " > " + FILEPATH + argvCheckT;
+  commandLaunchGen = FILEPATH + "./" + genName + " > " + FILEPATH + argvGenT;
 
-  commandRmWork = "rm " + workName;
-  commandRmCheck = "rm " + checkName;
-  commandRmGen = "rm " + genName;
+  commandRmWork = "rm " + FILEPATH + workName;
+  commandRmCheck = "rm " + FILEPATH + checkName;
+  commandRmGen = "rm " + FILEPATH + genName;
 
   std::stringstream workR, checkR;
 
@@ -63,14 +89,14 @@ int main(int argc, char *argv[]) {
   std::system(commandCompileCheck.c_str());
   std::system(commandCompileGen.c_str());
   std::cout << "Compiled\n";
+
   for (int i = 0; i < iterations; i++) {
     std::system(commandLaunchGen.c_str());
     std::system(commandLaunchWork.c_str());
     std::system(commandLaunchCheck.c_str());
     workR << workOutput.rdbuf();
     checkR << checkOutput.rdbuf();
-    while (workR.rdbuf()->in_avail() != 0 &&
-           checkR.rdbuf()->in_avail() != 0) {
+    while (workR.rdbuf()->in_avail() != 0 && checkR.rdbuf()->in_avail() != 0) {
       std::string readSomeWork, readSomeCheck;
       workR >> readSomeWork;
       checkR >> readSomeCheck;
@@ -81,6 +107,7 @@ int main(int argc, char *argv[]) {
     }
     std::cout << "Test " << i + 1 << " passed\n";
   }
+
 breakFlag:
   std::system(commandRmWork.c_str());
   std::system(commandRmCheck.c_str());
